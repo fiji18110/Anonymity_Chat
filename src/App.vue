251b94a72618bbc91ea3d3e4c7dfe71b48dd5666
fiji <ikuy.s.fw11@gmail.com>
2020-03-02 20:28:18 +0900

    Adjusted for use

--- src/App.vue
+++ src/App.vue
@@ -1,115 +1,70 @@
 <template>
   <div id="app">
-    <header class="header">
-      <div class="header__inner">
-        <template v-if="isSignedIn && userName && userPic">
-          <div class="header__user-image"><img :src='userPic'></div>
-          <p class="header__user-name">{{ userName }}</p>
-        </template>
-        <button class="header__auth-button" @click="authFunction">
-          {{ authButtonText }}
-        </button>
-      </div>
-    </header>
+    <!-- 名前の入力欄 -->
+    <label for="nameInput">名前</label>
+    <input type="txt" id="nameInput" v-model="name">
 
-    <router-view
-      :isSignedIn="isSignedIn"
-      :userName="userName"
-      :userPic="userPic"
-    >
-    </router-view>
+    <!-- メッセージの入力欄 -->
+    <label for="nameInput">メッセージ</label>
+    <input type="txt" id="nameInput" v-model="message">
 
+    <!-- ボタン類 -->
+    <div>
+      <button type="button" class="btn btn-default" @click="sendMessage">
+        送信
+      </button>
+    </div>
+
+    <!-- リスト -->
+    <div>
+      <ul>
+        <li v-for="(item,key) in list" :key="key">{{item.name}} / {{item.message}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
-  name: 'App',
+  name: 'app',
   data () {
     return {
-      userName: null,
-      userPic: null,
-      isSignedIn: null,
-      authButtonText: null,
-      authFunction: function () {}
+      list: [], // 最新状態はここにコピーされる
+      name: '', // 名前
+      message: ''// 送信メッセージ
     }
   },
   created () {
-    this.onAuthStateChanged()
+    this.listen()
   },
   methods: {
-    onAuthStateChanged () {
-      firebase.auth().onAuthStateChanged( user => {
-        this.userName = user ?
-          this.getUserName() : null
-        this.userPic = user ?
-          this.getProfilePicUrl() : null
-        this.authButtonText = user ?
-          'Sign-out' : 'Sign-in with Google'
-        this.authFunction = user ?
-          this.signOut : this.signIn
-        this.isSignedIn = user ?
-          true : false
+    // データベースの変更を購読、最新状態をlistにコピーする
+    listen () {
+      firebase.database().ref('myBoard/').on('value', snapshot => { // eslint-disable-line
+        if (snapshot) {
+          const rootList = snapshot.val()
+          let list = []
+          Object.keys(rootList).forEach((val, key) => {
+            rootList[val].id = val
+            list.push(rootList[val])
+          })
+          this.list = list
+        }
       })
     },
-    signIn () {
-      const provider = new firebase.auth.GoogleAuthProvider()
-      firebase.auth().signInWithPopup(provider)
-    },
-    signOut () {
-      firebase.auth().signOut()
-    },
-    isUserSignedIn () {
-      return !!firebase.auth().currentUser || false;
-    },
-    getProfilePicUrl () {
-      return firebase.auth().currentUser.photoURL || '/static/images/profile_placeholder.png';
-    },
-    getUserName () {
-      return firebase.auth().currentUser.displayName;
+    sendMessage () {
+      // 空欄の場合は実行しない
+      if (!this.name || !this.message) return
+      // 送信
+      firebase.database().ref('myBoard/').push({
+        name: this.name,
+        message: this.message
+      })
+      // 送信後inputを空にする
+      this.name = ''
+      this.message = ''
     }
   }
 }
 </script>
-
-<style lang="scss">
-  body {
-    background-color: #ECEFF1;
-  }
-  .header {
-    background-color: #009CE5;
-    &__inner {
-      width: 90%;
-      height: 70px;
-      margin: 0 auto;
-      display: flex;
-      align-items: center;
-    }
-    &__user-image {
-      position: relative;
-      width: 50px;
-      height: 50px;
-      overflow: hidden;
-      border-radius: 50%;
-      img {
-        width: 100%;
-        height: auto;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translateX(-50%) translateY(-50%);
-      }
-    }
-    &__user-name {
-      margin: 0 15px;
-      color: #FFF;
-    }
-    &__auth-button {
-      background: #FFF;
-      border: none;
-      border-radius: 50px;
-      padding: 5px 10px;
-      color: #009CE5;
-    }
-  }
-</style>
